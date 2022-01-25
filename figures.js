dc9b4f535c10c93cceb64ce07a7f24aa48645bbb
marioRLeyva1 <mario_980615@hotmail.com>
2022-01-24 22:52:29 -0600

    Interfaz

---
+++ figures.js
@@ -0,0 +1,29 @@
+//Función Cuadrado
+function areaCuadrado(lado){
+    var area = lado*lado;
+    return area;
+}
+function perimetroCuadrado(lado){
+    var perimetro = lado * 4;
+    return perimetro;
+}
+
+//Función Triángulo
+function areaTriangulo(base, altura){
+    var area = (base * altura) / 2;
+    return area;
+}
+function perimetroTriangulo(base, lado1, lado2){
+    var perimetro = base + ladoa + ladob;
+    return perimetro;
+}
+
+//Función Circulo
+function areaCiruculo(radio){
+    var area = (radio * radio) * Math.PI;
+    return area;
+}
+function perimetroCirculo(diametro){
+    var perimetro = diametro * Math.PI;
+    return perimetro;
+}
